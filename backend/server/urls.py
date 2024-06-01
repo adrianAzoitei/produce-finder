b444ca16d8e406e857566b397235d565e82c9bbb
Adrian Azoitei <azo.adrian@gmail.com>
2024-06-01 10:13:59 +0200

    feat: introduce serialization

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # TODO: add uid path parameter
     path("detect", views.detect, name="detect"),
 ]
--- backend/server/views.py
+++ backend/server/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse, HttpRequest, HttpResponseBadRequest
+from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseBadRequest
 from server.models import Produce
+from django.core import serializers
 from django.views.decorators.http import require_http_methods
+from pprint import pprint
 
 @require_http_methods(["POST"])
 def detect(request: HttpRequest) -> HttpResponse:
     if "file" in request.FILES:
         with request.FILES["file"] as f:
-            print(f.read())
-        return HttpResponse()
+            print("ok!")
+        data = serializers.serialize('json', Produce.objects.filter(pk="2"))
+        return HttpResponse(data, content_type="application/json")
     else:
         return HttpResponseBadRequest("file field missing from form")
